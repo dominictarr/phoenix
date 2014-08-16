647ba674cead0c36a66a492a402bcb2133e1344c
Paul Frazee <pfrazee@gmail.com>
2014-08-16 16:29:33 -0500

    added search page mockup

--- js/backend/index.js
+++ js/backend/index.js
@@ -4,37 +4,41 @@ var fs = require('fs');
 function createServer(port) {
 	var server = http.createServer(function (req, res) {
 		function pathStarts(v) { return req.url.indexOf(v) === 0; }
+		function serve404() {  res.writeHead(404); res.end('Not found'); }
 		if (req.url == '/' || req.url == '/index.html') {
 			res.writeHead(200, {'Content-Type': 'text/html'});
-			return fs.createReadStream('./feeds.html').pipe(res);
+			return fs.createReadStream('./feeds.html').on('error', serve404).pipe(res);
 		}
 		if (pathStarts('/profile/')) {
 			res.writeHead(200, {'Content-Type': 'text/html'});
-			return fs.createReadStream('./profile.html').pipe(res);
+			return fs.createReadStream('./profile.html').on('error', serve404).pipe(res);
+		}
+		if (pathStarts('/search/')) {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			return fs.createReadStream('./search.html').on('error', serve404).pipe(res);
 		}
 		if (pathStarts('/js/')) {
 			res.writeHead(200, {'Content-Type': 'application/javascript'});
-			return fs.createReadStream('./'+req.url).pipe(res);
+			return fs.createReadStream('./'+req.url).on('error', serve404).pipe(res);
 		}
 		if (pathStarts('/css/')) {
 			res.writeHead(200, {'Content-Type': 'text/css'});
-			return fs.createReadStream('./'+req.url).pipe(res);
+			return fs.createReadStream('./'+req.url).on('error', serve404).pipe(res);
 		}
 		if (pathStarts('/img/')) {
 			res.writeHead(200, {'Content-Type': 'image/png'});
-			return fs.createReadStream('./'+req.url).pipe(res);
+			return fs.createReadStream('./'+req.url).on('error', serve404).pipe(res);
 		}
 		if (pathStarts('/fonts/')) {
 			if (req.url.slice(-4) == 'woff') {
 				res.writeHead(200, {'Content-Type': 'application/x-font-woff'});
-				return fs.createReadStream('./'+req.url).pipe(res);
+				return fs.createReadStream('./'+req.url).on('error', serve404).pipe(res);
 			} else if (req.url.slice(-5) == 'woff2') {
 				res.writeHead(200, {'Content-Type': 'application/font-woff2'});
-				return fs.createReadStream('./'+req.url).pipe(res);
+				return fs.createReadStream('./'+req.url).on('error', serve404).pipe(res);
 			}
 		}
-		res.writeHead(404);
-		res.end('Not found');
+		serve404();
 	});
 	server.listen(port, '127.0.0.1');
 	return server;
