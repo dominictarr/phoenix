cc4eb0564514cab536100985d5db547eb7a50b0f
Paul Frazee <pfrazee@gmail.com>
2014-08-29 16:49:18 -0500

    added phoenix-relay deploy cli

--- js/common/ssb.js
+++ js/common/ssb.js
@@ -1,4 +1,7 @@
 var opts = require('secure-scuttlebutt/defaults');
-var SSB = require('secure-scuttlebutt');
+var SSB  = require('secure-scuttlebutt');
+var keys = require('./keys');
 module.exports = SSB(require('./db'), opts);
-module.exports.userFeed = module.exports.createFeed(require('./keys'));
+if (keys.exist) {
+	module.exports.userFeed = module.exports.createFeed(require('./keys'));
+}
