dd3bcff6a8bfe661be2592ee13b62e0d89d33833
Paul Frazee <pfrazee@gmail.com>
2014-08-29 12:20:28 -0500

    some cli UX work

--- js/daemon.js
+++ js/daemon.js
@@ -1,10 +1,10 @@
-if (false) {
+// if (false) {
 	// :TODO: do this when run as a daemon
 	var log = require('fs').createWriteStream(require('path').join(__dirname, '../phoenixd.log'), {'flags': 'a'});
 	process.__defineGetter__('stdout', function() { return log; });
 	process.__defineGetter__('stderr', function() { return log; });
 	process.on('uncaughtException', onException);
-}
+// }
 
 var relayPort  = /*process.argv[2] ||*/ 64000;
 var webguiPort = /*process.argv[3] ||*/ 65000;
