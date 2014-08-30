0eafe4b3ff05066dec8bfa97de070cfd71cfd9d1
Paul Frazee <pfrazee@gmail.com>
2014-08-29 23:58:29 -0500

    bugfixes in setup and deploy

--- js/common/ssb.js
+++ js/common/ssb.js
@@ -2,6 +2,7 @@ var opts = require('secure-scuttlebutt/defaults');
 var SSB  = require('secure-scuttlebutt');
 var keys = require('./keys');
 module.exports = SSB(require('./db'), opts);
+module.exports.createUserFeed = function() { module.exports.userFeed = module.exports.createFeed(require('./keys')); };
 if (keys.exist) {
-	module.exports.userFeed = module.exports.createFeed(require('./keys'));
+	module.exports.createUserFeed();
 }
