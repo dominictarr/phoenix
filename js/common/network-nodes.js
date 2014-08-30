af258583d4104877c2ab75f443263425c4cc7899
Paul Frazee <pfrazee@gmail.com>
2014-08-29 23:39:41 -0500

    local servers table now updates after deploy

--- js/common/network-nodes.js
+++ js/common/network-nodes.js
@@ -3,10 +3,10 @@ var pl = require('pull-level');
 var netNodesDB = db.sublevel('netnodes', { valueEncoding: 'json' });
 
 exports.add = function(addr, port, cb) {
-	netNodesDB.put([addr, port], [], cb);
+	netNodesDB.put([addr, +port], [], cb);
 };
 exports.del = function(addr, port, cb) {
-	netNodesDB.del([addr, port], cb);
+	netNodesDB.del([addr, +port], cb);
 };
 exports.createListStream = function(cb, opts) {
 	return pl.read(netNodesDB, opts);
