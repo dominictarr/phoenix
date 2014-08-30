7e3351df3d9ee7fd4d8a24bb493965644749ceb3
Paul Frazee <pfrazee@gmail.com>
2014-08-29 23:42:32 -0500

    changed user data folder to ~/.phoenix

--- js/common/config.js
+++ js/common/config.js
@@ -1,6 +1,6 @@
 var osenv    = require('osenv');
 var path     = require('path');
 
-exports.sbhome   = path.join(osenv.home(), '.scuttlebutt');
+exports.sbhome   = path.join(osenv.home(), '.phoenix');
 exports.namefile = path.join(exports.sbhome, 'secret.name');
 exports.dbpath   = path.join(exports.sbhome, 'database');
