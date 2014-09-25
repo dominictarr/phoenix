8456c8f55e3701b568288faecdead1943778f4a6
Jehan <jehan.tremback@gmail.com>
2014-09-24 21:09:52 -0700

    janky md support

--- js/web/feed.js
+++ js/web/feed.js
@@ -1,17 +1,21 @@
 $(function() {
   $('form.publisher textarea').on('keyup', togglePostBtn)
+  $('form.publisher textarea').on('keyup', markdownPreview)
   togglePostBtn({ target: $('form.publisher textarea') })
 
+  // yeeaaaa probably should refactor the frontend
+  $('.feed .content p:nth-child(2)').each(function (index) {
+    $(this).html(markdown.toHTML($(this).html()));
+  });
 
   function togglePostBtn(e) {
     //ctrl-enter to post when inside textarea
-    console.log(e.keyCode, e.ctrlKey)
+
     if(e.keyCode == 13 && e.ctrlKey)
       return $('form.publisher button').click()
 
     var $target = $(e.target);
     var str = $target.val() || ''
-    console.log('got', str)
     if (str.trim()) {
       $('form.publisher .btn').removeClass('hide')
       $target.attr('rows', 3)
@@ -19,6 +23,13 @@ $(function() {
       $('form.publisher .btn').addClass('hide')
       $target.attr('rows', 1)
     }
+  }
+
+  function markdownPreview (e) {
+    // Render markdown in preview panel
+    var preview = markdown.toHTML(e.target.value);
+
+    $('.js-preview-panel').html(preview);
   }
 
   $('.add-btn').on('click', function() {
