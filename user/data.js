b9cc5b20b91b4b62a06aedc52ca6e6bbbb661db1
Paul Frazee <pfrazee@gmail.com>
2014-11-21 15:29:12 -0600

    replaced user/demo.js with a more useful user/data.js page

--- user/data.js
+++ user/data.js
@@ -1,18 +1,13 @@
 var pull = require('pull-stream')
 var JSONH = require('json-human-buffer')
 
-document.body.innerHTML += '<h1 style="margin-top:0">Your Feed</h1>'
-document.body.innerHTML += '<p><button onclick="post()">Post "hello from demo.js" to your feed</button></p>'
-
-window.post = function () {
-  phoenix.add({ type: 'post', postType: 'text', text: 'hello from demo.js' })
-}
-
 pull(
   phoenix.createFeedStream(),
   pull.drain(function(msg) {
     // add to doc
-    document.body.innerHTML += '<pre>'+JSONH.stringify(msg, null, 2).replace(/</g, '&lt;').replace(/>/, '&gt;')+'</pre>'
+    var pre = document.createElement('pre')
+    pre.textContent = JSONH.stringify(msg, null, 2).replace(/</g, '&lt;').replace(/>/, '&gt;')
+    document.body.insertBefore(pre, document.body.firstChild)
   }, function() {
     // resize iframe
     var body = document.body,
