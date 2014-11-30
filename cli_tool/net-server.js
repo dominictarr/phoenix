7dbdd9945573a3029433fd09f653276323d3d0e1
Paul Frazee <pfrazee@gmail.com>
2014-11-30 11:21:57 -0600

    removed daemon mode

--- cli_tool/net-server.js
+++ cli_tool/net-server.js
@@ -6,23 +6,8 @@ var connect = require('../lib/backend');
 exports.start = function(opts) {
   var port = opts.port = opts.port || cfg.port
 
-  if (opts.daemon) {
-    // Daemon-mode
-    var argv = [''+port];
-    if (opts.config)
-      argv.push('--config'), argv.push(opts.config);
-    var daemon = require("daemonize2").setup({
-      main: "../net_server/daemon.js",
-      name: "phoenix-server",
-      pidfile: path.join(cfg.datadir, "./phoenix-server.pid"),
-      argv: argv
-    });
-    daemon.start();
-  } else {
-    // FG mode
-    require('../net_server/home').createServer(port, opts);
-    console.log('Phoenix Home Server....listening privately on localhost:' + port);
-  }
+  require('../net_server/home').createServer(port, opts);
+  console.log('Phoenix Home Server....listening privately on localhost:' + port);
 }
 
 exports.stop = function(opts) {
