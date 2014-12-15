4ba6f3163d7ddfe088e0a20844d2663aac34e6a5
Paul Frazee <pfrazee@gmail.com>
2014-12-15 17:47:25 -0600

    remove all old js

--- src/lib/com/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  suggestBox:  require('./suggest-box').suggestBox,
-  publishForm: require('./publish-form').publishForm,
-  message:     require('./message').message,
-  helptip:     require('./helptip').helptip
-}
