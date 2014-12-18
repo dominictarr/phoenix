a2e82c3a83c90728a948c835b4811c866428f2cb
Paul Frazee <pfrazee@gmail.com>
2014-12-17 18:22:55 -0600

    feed now renders newest to oldest

--- src/gui/pages/feed.js
+++ src/gui/pages/feed.js
@@ -3,13 +3,16 @@ var pull = require('pull-stream')
 var com = require('../com')
 
 module.exports = function(state) {
+  var msgs = []
+  for (var i=state.msgs.length-1; i>=0; i--) {
+    msgs.push(com.message(state, state.msgs[i]))
+  }
+
   var content = com.page(state, 'feed', h('.row',
     h('.col-xs-1', com.sidenav(state)),
     h('.col-xs-7', 
       com.postForm(state),
-      h('.message-feed', state.msgs.map(function(msg) {
-        return com.message(state, msg)
-      }))
+      h('.message-feed', msgs)
     )
   ))
 
