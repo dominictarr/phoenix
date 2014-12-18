e9a93c495a8af577be12f39df0217e97193b04dd
Paul Frazee <pfrazee@gmail.com>
2014-12-18 13:15:22 -0600

    split out some handlers into their own file

--- src/gui/handlers/index.js
+++ src/gui/handlers/index.js
@@ -1,47 +1,8 @@
-var util = require('../../lib/util')
-var markdown = require('../../lib/markdown')
-var com = require('../com')
-
 module.exports = {
-  'submit-publish-text-post': function(state, el, e) {
-    var form = (el.tagName == 'FORM') ? el : el.form
-    var text = form.text.value
-    var parent = form.dataset.parent
-
-    if (parent) state.apis.feed.postReply(text, parent, done)
-    else state.apis.feed.postText(text, done)
-    function done(err) {
-      if (err) alert(err.message)
-      else {
-        form.text.value = ''
-        var previewEl = form.querySelector('.preview')
-        previewEl.innerHTML = ''
-        state.sync()
-      }
-    }
-  },
-  'click-preview-text-post': function(state, el, e) {
-    var form = (el.tagName == 'FORM') ? el : el.form
-    var text = form.text.value
-
-    var previewEl = form.querySelector('.preview')
-    previewEl.style.display = 'block'
-    previewEl.innerHTML = markdown.block(util.escapePlain(text), state.nicknames)
-  },
-  'click-reply': function(state, el, e) {
-    var messageEl = el.parentNode.parentNode.parentNode
-    if (!messageEl.nextSibling || !messageEl.nextSibling.classList || !messageEl.nextSibling.classList.contains('reply-form')) {
-      var formEl = com.postForm(state, el.dataset.msgid)
-      if (messageEl.nextSibling)
-        messageEl.parentNode.insertBefore(formEl, messageEl.nextSibling)
-      else
-        messageEl.parentNode.appendChild(formEl)
-    }
-  },
-  'click-cancel-reply': function(state, el, e) {
-    var replyFormEl = el.parentNode.parentNode
-    replyFormEl.parentNode.removeChild(replyFormEl)
-  },
+  'submit-publish-text-post': require('./textpost').submit,
+  'click-preview-text-post': require('./textpost').preview,
+  'click-reply': require('./textpost').reply,
+  'click-cancel-reply': require('./textpost').cancelReply,
   'click-react': function(state, el, e) {
     var text = prompt('What is your reaction? eg "likes", "agrees with"')
     if (!text)
---
+++ src/gui/handlers/textpost.js
@@ -0,0 +1,46 @@
+var com = require('../com')
+var util = require('../../lib/util')
+var markdown = require('../../lib/markdown')
+
+exports.submit = function(state, el, e) {
+  var form = (el.tagName == 'FORM') ? el : el.form
+  var text = form.text.value
+  var parent = form.dataset.parent
+
+  if (parent) state.apis.feed.postReply(text, parent, done)
+  else state.apis.feed.postText(text, done)
+  function done(err) {
+    if (err) alert(err.message)
+    else {
+      form.text.value = ''
+      var previewEl = form.querySelector('.preview')
+      previewEl.innerHTML = ''
+      state.sync()
+    }
+  }
+}
+
+exports.preview = function(state, el, e) {
+  var form = (el.tagName == 'FORM') ? el : el.form
+  var text = form.text.value
+
+  var previewEl = form.querySelector('.preview')
+  previewEl.style.display = 'block'
+  previewEl.innerHTML = markdown.block(util.escapePlain(text), state.nicknames)
+}
+
+exports.reply = function(state, el, e) {
+  var messageEl = el.parentNode.parentNode.parentNode
+  if (!messageEl.nextSibling || !messageEl.nextSibling.classList || !messageEl.nextSibling.classList.contains('reply-form')) {
+    var formEl = com.postForm(state, el.dataset.msgid)
+    if (messageEl.nextSibling)
+      messageEl.parentNode.insertBefore(formEl, messageEl.nextSibling)
+    else
+      messageEl.parentNode.appendChild(formEl)
+  }
+}
+
+exports.cancelReply = function(state, el, e) {
+  var replyFormEl = el.parentNode.parentNode
+  replyFormEl.parentNode.removeChild(replyFormEl)
+}
