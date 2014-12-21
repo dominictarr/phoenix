4d0a14cf447b56e98a553e32ff11c8b0b044eb17
Paul Frazee <pfrazee@gmail.com>
2014-12-21 16:31:23 -0600

    added not found ui for users

--- src/gui/pages/profile.js
+++ src/gui/pages/profile.js
@@ -6,25 +6,27 @@ var util = require('../../lib/util')
 module.exports = function(state) {
   var pid = state.page.param
   var profile = state.profiles[pid]
-
-  if (!profile) {
-    // :TODO: profile not found
-    return state.setPage(com.page(state, 'profile', h('.row',
-      h('.col-xs-1', com.sidenav(state)),
-      h('.col-xs-7', 'Not found (todo)')
-    )))
-  }
+  var isFollowing = (state.user.following.indexOf(pid) != -1)
 
   // render messages
-  var msgs = []
+  var msgfeed, msgs = []
   for (var i=state.msgs.length-1; i>=0; i--) {
     if (state.msgs[i].value.author == pid)
       msgs.push(com.message(state, state.msgs[i]))
   }
+  if (msgs.length)
+    msgfeed = h('.message-feed', msgs)
+  else {
+    msgfeed = h('p', 
+      'No messages found for this user.',
+      ((!isFollowing) ? 
+        h('p.text-muted', 'Follow this user to begin searching the network for their data.') :
+        h('p.text-muted', 'Scuttlebutt is searching the network for this user.'))
+    )
+  }
 
   // render controls
   var followBtn = '', setNickBtn = ''
-  var isFollowing = (state.user.following.indexOf(pid) != -1)
   if (pid == state.user.id) {
     setNickBtn = h('button.btn.btn-default.click-set-nickname', 'Set Nickname')
   } else {
@@ -36,13 +38,13 @@ module.exports = function(state) {
   } 
 
   // render page
-  var nickname = state.nicknames[pid]
-  var joinDate = new Date(profile.createdAt)
+  var nickname = state.nicknames[pid] || util.shortString(pid)
+  var joinDate = (profile) ? util.prettydate(new Date(profile.createdAt), true) : '-'
   state.setPage(com.page(state, 'profile', h('.row',
     h('.col-xs-1', com.sidenav(state)),
-    h('.col-xs-7', h('.message-feed', msgs)),
+    h('.col-xs-7', msgfeed),
     h('.col-xs-4',
-      h('h2', nickname, ' ', h('small', 'joined '+util.prettydate(joinDate, true))),
+      h('h2', nickname, ' ', h('small', 'joined '+joinDate)),
       h('p', followBtn, ' ', setNickBtn),
       h('small', 'EmojID:'), h('br'),
       h('div', { style: { width: '160px' }, innerHTML: com.toEmoji(pid) })
