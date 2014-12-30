6fc6cf1fd4e92561a21bc7aa79c0743d8a4df496
Paul Frazee <pfrazee@gmail.com>
2014-12-29 18:30:56 -0600

    updates to help docs

--- src/gui/pages/help.js
+++ src/gui/pages/help.js
@@ -3,23 +3,25 @@ var com = require('../com')
 
 module.exports = function(state) {
   var content
-  if (state.page.param == 'networking') {
+  if (state.page.param == 'apps') {
     content = [
-      panel('Contacts', ['If you want to follow somebody and receive their broadcasts and messages, you have to add them as a contact. They\'ll have to do the same if they want to see your messages. ', h('strong', 'TODO: how to add contacts')]),
-      panel('Pub Servers', 'Phoenix uses "pub servers" to get messages across the network. Pub servers are simplistic: all they do is receive your messages and host them for other people to fetch. Since they\'re on the public web and always on, they help the network stay available.'),
-      panel('Sync', 'To get the latest messages, you need to ask the servers in your network for the latest (kind of like with email). This happens periodically in the background, but, if you want to force a sync, you can press the big Sync button in the top right.')
+      panel('Applications', 'Web applications can detect that you\'re running Scuttlebutt and ask for access. This is a beta feature, so be careful! Once an app can read or write to your feed, it can post messages freely. (It\'s recommended that only developers use apps at this time.)'),
+      panel('Developers', 'If you want to write applications with Scuttlebutt, visit THIS DOCUMENTATION THAT DOESNT EXIST (TODO).'),
+      panel('De-authorizing Apps', 'If you want to revoke access to Scuttlebutt, restart your ssb process. App authorizations are only stored in memory, and so this will clear the permissions records.')
     ]
-  } else if (state.page.param == 'privacy') {
+  } else if (state.page.param == 'networking') {
     content = [
-      panel('How Phoenix Works', 'Phoenix is built on a "distributed" messaging system, like email. Users and their messages are identified by unique, secure keys, allowing us to share messages between devices without relying on central servers.'),
-      panel('Is it Private?', 'Phoenix v1 is only a public broadcast system, like Twitter. In future versions, we\'ll add encryption and direct messages so that you can share messages with individuals and groups.')
+      panel('Contacts', 'Scuttlebutt searches the network for messages from your contacts, plus messages from the people your contacts follow. If you want to be sure you get a specific persons\'s messages, follow their ID.'),
+      panel('Following Users', 'To follow somebody, find their profile page and hit the "Follow" button. If you have their ID, but not their profile page, you can hit the "Add contact" button on the top right and enter the ID in the popup.'),
+      panel('Pub Servers', 'Scuttlebutt uses "pub servers" to get messages across the network. Pub servers are bots that receive your messages and host them for other people to fetch. Since they\'re on the public Web and always on, they help the network stay available. You\'ll need to use a pub server if you want to reach people outside of your LAN.'),
+      panel('Invite Codes', 'Pub servers are like any other users, and they only accept the messages from friends and friends-of-friends. If someone you know is running a pub server, ask them for an invite code. You can use the code by pasting it into the "Add contact" dialog, just like when following somebody.'),
+      panel('Running a Pub Server', ['If you want to run your own pub server, ', com.a('https://github.com/ssbc/scuttlebot#running-your-own-pub-server', 'follow the instructions in the scuttlebot repo'), '. Note, this is for advanced users.'])
     ]
   } else {
     content = [
-      panel('Basics', 'Phoenix v1 is a social feed application. You can broadcast to everyone following you, much like on Twitter. However, unlike Twitter, only people you are following can contact you. This means no unsolicited spam!'),
-      panel('Replies', 'You can reply to other users with text posts and reactions. When you open a message\'s page, you\'ll see all of the replies in a threaded view, like a message-board. Click the age of a post (eg "5m", "yesterday") to get to its page.'),
-      panel('Sharing', 'If you want to spread somebody\'s message, you can "share" it to your followers. This is just like retweeting, except, in the case of Phoenix, it\'s the ONLY way to spread a message. If somebody replies to you, your non-mutual followers will only see that reply if you share it.'),
-      panel('Mentions', ['Like in most social networks, you can "@-mention" other users. If they follow you, or if somebody shares the message to them, they\'ll be notified of the mention. Check your ', com.a('#/inbox', 'Inbox'), ' to find your notifications.']),
+      panel('Basics', 'Scuttlebutt is a free online network that runs on user devices. That means there\'s no .com running the show! Your computers stay in sync automatically by connecting over the LAN and public Web.'),
+      panel('Posts', ['Posts in Scuttlebot are formatted in ', com.a('https://en.wikipedia.org/wiki/Markdown', 'Markdown'), '. Hit the Preview button to see your message. Be warned, once you press the "Post" button, there is no undo or delete!']),
+      panel('Mentions', ['Like in most social networks, you can "@-mention" other users. When they receive the message, they\'ll be notified of the mention. Check your ', com.a('#/inbox', 'Inbox'), ' to find your notifications.']),
       panel('Emojis', ['You can put emojis in your posts using colons. For instance, \':smile:\' will result in ', h('img.emoji', { src: '/img/emoji/smile.png', height: 20, width: 20}), '. Check the ', com.a('http://www.emoji-cheat-sheet.com/', 'Emoji Cheat Sheet'), ' to see what\'s available'])
     ]
   }
@@ -30,7 +32,7 @@ module.exports = function(state) {
     h('.col-xs-3.col-md-4', h('ul.nav.nav-pills.nav-stacked', helpnav('#/help/'+state.page.param, [
       ['#/help/intro', 'Getting Started'],
       ['#/help/networking', 'Networking'],
-      ['#/help/privacy', 'Privacy']
+      ['#/help/apps', '3rd-Party Apps']
     ])))
   )))
 }
