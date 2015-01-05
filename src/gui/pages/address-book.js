294777a954d0a3e7a5930129a329eea114522a7b
Paul Frazee <pfrazee@gmail.com>
2015-01-05 12:11:34 -0600

    address book style tweaks

--- src/gui/pages/address-book.js
+++ src/gui/pages/address-book.js
@@ -6,18 +6,17 @@ module.exports = function(state) {
   state.setPage(com.page(state, 'network', h('.row',
     h('.col-xs-2.col-md-1', com.sidenav(state)),
     h('.col-xs-8',
-      h('h3', {style:'margin-top:0'}, 'Address Book'),
-      h('table.table.table-bordered',
-        h('thead', h('tr', h('th', 'Name'), h('th', {width:'70'}, 'Follow'), h('th', {width:'70'}, 'Trust'))),
+      h('table.table',
+        h('thead', h('tr', h('th', 'Name'), h('th.text-center', {width:'70'}, 'Follow'), h('th.text-center', {width:'70'}, 'Trust'))),
         h('tbody',
           Object.keys(state.profiles).map(function(id) { 
             var profile = state.profiles[id]
             return h('tr',
               h('td', 
+                h('button.btn.btn-primary.btn-sm', {title: 'Rename'}, com.icon('pencil')), ' ',
                 h('strong', com.a('#/profile/'+id, state.names[id])),
                 ' ', h('small.text-muted', 'aka bob ', h('strong', 'x5'), ' robert ', h('strong', 'x2')),
-                ' ', (~state.user.followers.indexOf(id)) ? h('span.label.label-success', 'follows you') : '',
-                h('button.btn.btn-primary.btn-sm.pull-right', {title: 'Change Name'}, com.icon('pencil'))
+                ' ', (~state.user.followers.indexOf(id)) ? h('span.label.label-success', 'follows you') : ''
               ),
               h('td.text-center', 
                 (~state.user.following.indexOf(id))
