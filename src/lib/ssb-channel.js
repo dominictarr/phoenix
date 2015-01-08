56768b1abd8ac1b79f9f552708577552c864c739
Paul Frazee <pfrazee@gmail.com>
2015-01-08 12:49:26 -0600

    factored out ssb-address module

--- src/lib/ssb-channel.js
+++ src/lib/ssb-channel.js
@@ -1,27 +1,7 @@
 var pull = require('pull-stream')
 var ws   = require('pull-ws-server')
 var EventEmitter = require('events').EventEmitter
-
-var DEFAULT_PROTOCOL = 'http:'
-var DEFAULT_PORT = 2000
-
-function address(addr) {
-  addr = addr || {}
-  if (typeof addr == 'string'){
-    var parts = addr.split(':')
-    if (parts.length === 3)
-      addr = { protocol: parts[0]+':', host: parts[1].slice(2), port: parts[2]}
-    else if (parts .length === 2)
-      addr = { host: parts[0], port: parts[1] }
-    else
-      addr = { host: parts[0] }
-  }
-  if (!addr.protocol) addr.protocol = DEFAULT_PROTOCOL
-  if (!addr.host) throw new Error('BadParam - no host:String or {.host:String}')
-  if (!addr.port || +addr.port != addr.port) addr.port = DEFAULT_PORT
-  addr.domain = addr.protocol+'//'+addr.host+':'+addr.port
-  return addr
-}
+var address = require('ssb-address')
 
 exports.connect = function (rpcapi, addr, cb) {
   var chan = new EventEmitter()
--- src/lib/ssb-domain-auth.js
+++ src/lib/ssb-domain-auth.js
@@ -1,27 +1,8 @@
-var querystr   = require('querystring')
+var querystr = require('querystring')
+var address = require('ssb-address')
 
-var DEFAULT_PROTOCOL = 'http:'
-var DEFAULT_PORT = 2000
 var ACCESS_PATH = '/access.json'
 var AUTH_PATH = '/auth.html'
-
-function address(addr) {
-  addr = addr || {}
-  if (typeof addr == 'string'){
-    var parts = addr.split(':')
-    if (parts.length === 3)
-      addr = { protocol: parts[0]+':', host: parts[1].slice(2), port: parts[2]}
-    else if (parts .length === 2)
-      addr = { host: parts[0], port: parts[1] }
-    else
-      addr = { host: parts[0] }
-  }
-  if (!addr.protocol) addr.protocol = DEFAULT_PROTOCOL
-  if (!addr.host) throw new Error('BadParam - no host:String or {.host:String}')
-  if (!addr.port || +addr.port != addr.port) addr.port = DEFAULT_PORT
-  addr.domain = addr.protocol+'//'+addr.host+':'+addr.port
-  return addr
-}
 
 module.exports = {
   getToken: function(addr, cb) {
