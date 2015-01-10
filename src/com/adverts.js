b06a156d5ef9f93ac0f506e2d7e5ec2a05b34d64
Paul Frazee <pfrazee@gmail.com>
2015-01-10 17:21:25 -0600

    update remaining com to use app/api pattern

--- src/com/adverts.js
+++ src/com/adverts.js
@@ -3,22 +3,19 @@ var com = require('./index')
 var util = require('../../lib/util')
 var markdown = require('../../lib/markdown')
 
-module.exports = function(state) {
-  var ads = []
-  for (var i=0; i < 2 && i < state.adverts.length; i++) {
-    var index = state.adverts.length - (Math.random()*Math.min(state.adverts.length, 30))|0
-    ads.push(state.msgsById[state.adverts[index]])
-  }
-  if (ads.length)
-    ads = ads.map(renderAd.bind(null, state))
-  else
-    ads = h('small', 'this space is reserved for user adverts - ', com.a('#/adverts', 'try it out!'))
-  return h('.adverts-but-its-cool-tho', ads)
+module.exports = function (app) {
+  var adspace = h('.adverts-but-its-cool-tho')
+  app.api.getRandomAdverts(3, 30, function (err, ads) {
+    if (ads.length)
+      adspace.appendChild(ads.map(renderAd.bind(null, app)))
+    else
+      adspace.appendChild(h('small', 'this space is reserved for user adverts - ', com.a('#/adverts', 'try it out!')))
+  })
 }
 
-function renderAd(state, ad) {
+function renderAd (app, ad) {
   return [
-    h('small', 'advert by ', com.userlink(ad.value.author, state.names[ad.value.author])),
-    h('.well.well-sm', { innerHTML: markdown.block(util.escapePlain(ad.markdown), state.names) })
+    h('small', 'advert by ', com.userlink(ad.value.author, app.getNameById(ad.value.author))),
+    h('.well.well-sm', { innerHTML: markdown.block(util.escapePlain(ad.markdown)) })
   ]
 }
