b4d733d0c6df22ae3bdeac70e6e8353d2ed1fea2
Paul Frazee <pfrazee@gmail.com>
2015-01-11 20:33:11 -0600

    added load-more btn to posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,10 +1,13 @@
 var h = require('hyperscript')
-var pull = require('pull-stream')
 var com = require('../com')
 
 module.exports = function (app) {
-  app.api.getPosts({ start: 0, end: 30 }, function (err, msgs) {
+  var opts = { start: 0 }
+  app.api.getPosts(opts, function (err, msgs) {
 
+    // markup
+
+    var content
     if (msgs.length === 0) {
       content = [
         h('hr'),
@@ -28,15 +31,34 @@ module.exports = function (app) {
     } else {
       content = h('table.table.message-feed', msgs.map(function (msg) { return com.messageSummary(app, msg) }))
     }
-
+   
+    var loadMoreBtn = (msgs.length === 30) ? h('p', h('button.btn.btn-primary', { onclick: loadMore }, 'Load More')) : ''
     app.setPage('posts', h('.row',
       h('.col-xs-2.col-md-1', com.sidenav(app)),
-      h('.col-xs-8', content),
+      h('.col-xs-8', 
+        content,
+        loadMoreBtn
+      ),
       h('.col-xs-2.col-md-3',
         com.adverts(app),
         h('hr'),
         com.sidehelp(app)
       )
     ))
+
+    // handlers
+
+    function loadMore (e) {
+      e.preventDefault()
+      opts.start += 30
+      app.api.getPosts(opts, function (err, moreMsgs) {
+        if (moreMsgs.length > 0) {
+          moreMsgs.forEach(function (msg) { content.appendChild(com.messageSummary(app, msg)) })
+        }
+        // remove load more btn if it looks like there arent any more to load
+        if (moreMsgs.length < 30)
+          loadMoreBtn.parentNode.removeChild(loadMoreBtn)
+      })
+    }
   })
 }
