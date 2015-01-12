15d9572acda9fc238f1516d6ec749741cd57af7e
Paul Frazee <pfrazee@gmail.com>
2015-01-11 20:46:18 -0600

    added load-more btn to feed page

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -4,15 +4,39 @@ var com = require('../com')
 
 var rawOpts = {raw: true}
 module.exports = function (app) {
-  app.api.getFeed({ limit: 30, reverse: true }, function (err, msgs) {
+  var opts = { limit: 30, reverse: true }
+  app.api.getFeed(opts, function (err, msgs) {
+    var lastMsg = msgs.slice(-1)[0]
+
+    // markup
+
+    var loadMoreBtn = (msgs.length === 30) ? h('p', h('button.btn.btn-primary', { onclick: loadMore }, 'Load More')) : ''
+    var content = h('.message-feed', msgs.map(function (msg) { return com.message(app, msg, rawOpts) }))
     app.setPage('feed', h('.row',
       h('.col-xs-2.col-md-1', com.sidenav(app)),
-      h('.col-xs-8', h('.message-feed', msgs.map(function (msg) { return com.message(app, msg, rawOpts) }))),
+      h('.col-xs-8', content, loadMoreBtn),
       h('.col-xs-2.col-md-3',
         com.adverts(app),
         h('hr'),
         com.sidehelp(app)
       )
     ))
+
+    // handlers
+
+    function loadMore (e) {
+      e.preventDefault()
+      opts.lt = lastMsg
+      app.api.getFeed(opts, function (err, moreMsgs) {
+        if (moreMsgs.length > 0) {
+          moreMsgs.forEach(function (msg) { content.appendChild(com.message(app, msg, rawOpts)) })
+          lastMsg = moreMsgs.slice(-1)[0]
+        }
+        // remove load more btn if it looks like there arent any more to load
+        if (moreMsgs.length < 30)
+          loadMoreBtn.parentNode.removeChild(loadMoreBtn)
+      })
+    }
+
   })
 }
