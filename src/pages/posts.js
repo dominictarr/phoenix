ce728f52789e25605b5e0499dede0a6e2aec6dbc
Paul Frazee <pfrazee@gmail.com>
2015-01-15 12:01:01 -0600

    posts-page getting started help now stays in footer

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -8,43 +8,38 @@ module.exports = function (app) {
 
     // markup
 
-    var content
-    if (msgs.length === 0) {
-      content = [
-        h('.row',
-          h('.col-xs-4',
-            com.panel(h('span', 'Join a Pub Server ', h('small', 'recommended')),
-              h('div',
-                h('p', 'Ask the owner of a pub server for an ', com.a('#/help/networking', 'invite code'), '.'),
-                h('p', h('button.btn.btn-primary', { onclick: app.followPrompt }, 'Use invite'))
-              )
-            )
-          ),
-          h('.col-xs-4',
-            com.panel('Connect over WiFi',
-              h('p', 'Open the ', com.a('#/address-book', 'address book'), ' and find peers on your WiFi in the ', h('strong', 'Network'), ' column.')
-            )
-          ),
-          h('.col-xs-4',
-            com.panel(h('span', 'Start a Pub Server ', h('small', 'advanced')),
-              h('p',
-                com.a('https://github.com/ssbc/scuttlebot#running-your-own-pub-server', 'Follow these instructions'),
-                ' then hand out invite codes to friends.'
-              )
-            )
+    var content = h('table.table.message-feed', msgs.map(function (msg) {
+      return com.messageSummary(app, msg)
+    }))
+
+    var help = h('.row',
+      h('.col-xs-4',
+        com.panel(h('span', 'Join a Pub Server ', h('small', 'recommended')),
+          h('div',
+            h('p', 'Ask the owner of a pub server for an ', com.a('#/help/networking', 'invite code'), '.'),
+            h('button.btn.btn-primary', { onclick: app.followPrompt }, 'Use invite')
           )
         )
-      ]
-    } else {
-      content = h('table.table.message-feed', msgs.map(function (msg) {
-        return com.messageSummary(app, msg)
-      }))
-    }
+      ),
+      h('.col-xs-4',
+        com.panel('Connect over WiFi',
+          h('p', 'Open the ', com.a('#/address-book', 'address book'), ' and find peers on your WiFi in the ', h('strong', 'Network'), ' column.')
+        )
+      ),
+      h('.col-xs-4',
+        com.panel(h('span', 'Start a Pub Server ', h('small', 'advanced')),
+          h('p',
+            com.a('https://github.com/ssbc/scuttlebot#running-your-own-pub-server', 'Follow these instructions'),
+            ' then hand out invite codes to friends.'
+          )
+        )
+      )
+    )
    
-    var loadMoreBtn = (msgs.length === 30) ? h('p', h('button.btn.btn-primary', { onclick: loadMore }, 'Load More')) : ''
+    var loadMoreBtn = (msgs.length === 30) ? h('p', h('button.btn.btn-primary', { onclick: loadMore, style: 'margin-bottom: 24px' }, 'Load More')) : ''
     app.setPage('posts', h('.row',
       h('.col-xs-2.col-md-1', com.sidenav(app)),
-      h('.col-xs-8', content, loadMoreBtn),
+      h('.col-xs-8', content, loadMoreBtn, help),
       h('.col-xs-2.col-md-3',
         com.adverts(app),
         h('hr'),
