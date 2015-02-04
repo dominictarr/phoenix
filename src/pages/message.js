063125d94e17974813886babb8a6ed22410c95ee
Dominic Tarr <dominic.tarr@gmail.com>
2015-02-05 10:34:07 +1300

    use generic secure-scuttlebutt methods instead of indexes. probably the post parent index is not necessary, since the key for the parent message is a property of current message.

--- src/pages/message.js
+++ src/pages/message.js
@@ -4,18 +4,25 @@ var com = require('../com')
 var util = require('../lib/util')
 
 module.exports = function (app) {
-  app.ssb.phoenix.getThread(app.page.param, function (err, thread) {
+  app.ssb.relatedMessages({
+    id: app.page.param, rel: 'replies-to',
+    count: true, parent: true
+  }, function (err, thread) {
     var content
     if (thread) {
       content = com.messageThread(app, thread, { fullLength: true })
-      app.ssb.phoenix.getPostParent(app.page.param, function (err, parent) {
-        if (parent) {
-          var pauthor = parent.value.author
-          var header = content.querySelector('.panel-heading .in-response-to')
-          header.appendChild(h('span', {innerHTML: ' &middot; in response to '}))
-          header.appendChild(com.a('#/msg/'+parent.key, 'a post by ' + (app.names[pauthor] || util.shortString(pauthor))))
-        }
-      })
+      try {
+        var pkey = thread.value.content.repliesTo.msg
+        app.ssb.get(pkey, function (err, parent) {
+          if (parent) {
+            var pauthor = parent.author
+            var header = content.querySelector('.panel-heading .in-response-to')
+            header.appendChild(h('span', {innerHTML: ' &middot; in response to '}))
+            header.appendChild(com.a('#/msg/'+pkey, 'a post by ' + (app.names[pauthor] || util.shortString(pauthor))))
+          }
+        })
+      }
+      catch (_) {}
     } else {
       content = 'Message not found.'
     }
