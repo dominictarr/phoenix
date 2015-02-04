b121038629116b07db612163867f80166017e3c6
Paul Frazee <pfrazee@gmail.com>
2015-02-04 15:28:42 -0800

    fix: only expect a parent post when there is one

--- src/pages/message.js
+++ src/pages/message.js
@@ -8,19 +8,21 @@ module.exports = function (app) {
     var content
     if (thread) {
       content = com.messageThread(app, thread, { fullLength: true })
-      app.ssb.phoenix.getPostParent(app.page.param, function (err, parent) {
-        var summary
-        if (parent) {
-          var pauthor = (app.names[parent.value.author] || util.shortString(parent.value.author))
-          if (parent.value.content.text)
-            summary = '^ ' + pauthor + ': "' + util.shortString(parent.value.content.text, 100) + '"'
-          else
-            summary = '^ '+parent.value.content.type+' message by ' + pauthor
-        } else {
-          summary = '^ parent message not yet received (' + thread.parent + ')'
-        }
-        content.querySelector('.in-response-to').appendChild(com.a('#/msg/'+thread.parent, summary))
-      })
+      if (thread.parent) {
+        app.ssb.phoenix.getPostParent(app.page.param, function (err, parent) {
+          var summary
+          if (parent) {
+            var pauthor = (app.names[parent.value.author] || util.shortString(parent.value.author))
+            if (parent.value.content.text)
+              summary = '^ ' + pauthor + ': "' + util.shortString(parent.value.content.text, 100) + '"'
+            else
+              summary = '^ '+parent.value.content.type+' message by ' + pauthor
+          } else {
+            summary = '^ parent message not yet received (' + thread.parent + ')'
+          }
+          content.querySelector('.in-response-to').appendChild(com.a('#/msg/'+thread.parent, summary))
+        })
+      }
     } else {
       content = 'Message not found.'
     }
